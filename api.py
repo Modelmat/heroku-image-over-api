90a869461cff90bb334ca092085f823bd026d7e6
Modelmat <Modelmat@users.noreply.github.com>
2018-08-13 18:59:48 +1000

    Add some safety checks for lage image files

--- api.py
+++ api.py
@@ -1,11 +1,31 @@
 import aiohttp
 from aiohttp import web
+import async_timeout
 import jsonpath_rw # pip install jsonpath-rw
 import traceback
 
+class URLTooLarge(Exception):
+    """The content-length headers was too large to reasonably download"""
+    
+class NoContentLengthHeader(Exception):
+    """Due to download restrictions a content-length header is required"""
+    
+def check_headers(headers):
+    if "content-length" not in headers:
+        raise NoContentLengthHeader
+    elif headers["content-length"] > 104857600:
+        raise URLTooLarge
+
 async def root_handled(request):
     try:
-        return await root(request)
+        async with async_timeout.timeout(15):
+            return await root(request)
+    except asyncio.TimeoutError:
+        return web.Response(body="Either the request URL or the image URL was unresponive")
+    except NoContentLengthHeader:
+        return web.Response(body=NoContentLengthHeader.__doc__)
+    except URLTooLarge:
+        return web.Response(body=URLTooLarge.__doc__)
     except:
         traceback.print_exc()
         return web.Response(body=traceback.format_exc())
@@ -15,6 +35,9 @@ async def root(request):
     expression = jsonpath_rw.parse(request.query["query"])
 
     async with aiohttp.ClientSession() as session:
+        async with session.head(_url) as resp:
+            check_headers(resp.headers)
+
         async with session.get(url) as resp:
             json = await resp.json()
 
@@ -33,6 +56,9 @@ async def root(request):
         if suffix:
             image_url += suffix
 
+        async with session.head(image_url) as resp:
+            check_headers(resp.headers)
+            
         async with session.get(image_url) as resp:
             body = await resp.read()
 
